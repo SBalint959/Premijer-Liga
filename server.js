fe49abfec7771745c1ebd9980b121d481cbe3d78
Sven Balint <sven.balint@fer.hr>
2022-10-28 15:30:48 +0200

    render deploy configurations

--- server.js
+++ server.js
@@ -23,4 +23,18 @@ app.use('/', indexRouter);
 app.use('/datatable', datatableRouter);
 //app.use('/api/v1/departments', apiRouter);
 
-app.listen(3000);
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 8080;
+
+if (externalUrl) {
+    const hostname = '127.0.0.1';
+    app.listen(port, hostname, () => {
+        console.log(`Server locally running at http://${hostname}:${port}/ and from outside on ${externalUrl}`);
+    });
+}
+else {
+    app.listen(port, () => {
+        console.log(`server started at http://localhost:${port}`);
+    });
+
+}
