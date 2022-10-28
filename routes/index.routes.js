9704a5594b8c00373aaaa02663358ee733c6d22f
Sven Balint <sven.balint@fer.hr>
2022-10-28 19:56:19 +0200

    Stranica komentara mozda

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -34,41 +34,6 @@ router.get('/profile', requiresAuth(), (req, res) => {
    }
 });
 
-router.get('/update', async function (req, res, next) {
-    if(req.oidc.user === undefined){
-        res.send("Access denied. You are not logged in!")
-    } else {
-        const sqlPodaci =  `SELECT * FROM topplayerteams NATURAL JOIN topplayers`;
-        try {
-            const resultPodaci = (await db.query(sqlPodaci, [])).rows;
-
-            fs.writeFile('./NLLTeams.json', JSON.stringify(resultPodaci, null, 3), (err) => {
-                if(err){
-                    console.log(err);
-                }
-            });
-            const parser = new Parser({
-                escapedQuote: '"'
-            });
-            try{
-                let csvFile = parser.parse(resultPodaci);
-                fs.writeFile('./NLLTeams.csv', csvFile,(err) => {
-                    if(err){
-                        console.log(err);
-                    }
-                });
-           }
-            catch(err){
-                console.log(err);
-            }
-            res.render('updatedFiles');
-
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-});
 
 router.get('/', function (req, res, next) {
     res.render('index', {
